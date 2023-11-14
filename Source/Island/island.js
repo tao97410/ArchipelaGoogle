ad3d4e256611bae09f63667d67b4b9a19839f032
Cognot Alban <acognot@2D-LINUX-140.insa-lyon.fr>
2023-11-14 12:14:20 +0100

    placements des données avec la nouvelle structure de json

--- Source/Island/island.js
+++ Source/Island/island.js
@@ -3,10 +3,11 @@
 var xhttp= new XMLHttpRequest();
 xhttp.onreadystatechange=SetPage;
 
-xhttp.open('GET','../../exemple.json');
+xhttp.open('GET','../../exemple2.json');
 
 xhttp.send();
 
+var information 
 
 
 function SetPage(island) {
@@ -14,10 +15,40 @@ function SetPage(island) {
         var tbody=document.getElementsByTagName("tbody");
         var information=JSON.parse(this.response);
         console.log(information);
-        var nomIle=document.getElementById('nomIle');
-        var statut=document.getElementById('Statut');
-        nomIle.innerHTML=information.entities.Q17070.labels.fr.value;
-        statut.innerHTML=information.entities.Q17070.descriptions.fr.value;
+        var nomIle_html=document.getElementById('Content_NomIle');
+        var statut_html=document.getElementById('Content_Statut');
+        var area_html = document.getElementById('Content_Area');
+        var population_html = document.getElementById('Content_Population');
+        var coordonates_html = document.getElementById('Content_Coordonates');
+
+        var archipel_html = document.getElementById('Content_Archipel');
+        var mer_html = document.getElementById('Content_Mers');
+        var pays_html = document.getElementById('Content_Pays');
+        
+        nomIle_html.innerHTML=information.Name;
+        statut_html.innerHTML=information.Desc;
+        area_html.innerHTML = information.Area;
+        population_html.innerHTML = information.Population;
+        coordonates_html.innerHTML=information.Coordonates.latitude + " " + information.Coordonates.longitude; 
+        archipel_html.innerHTML = information.Archipelago.Name;
+
+        // Initialize seaNames as an empty array
+        const seaNames = [];
+
+        // Extract names from each sea element and push them into the seaNames array
+        information.Seas.forEach(seaElement => {
+        seaNames.push(seaElement.Name);
+        });
+
+        // Join the seaNames array with commas to create a comma-separated list
+        const commaSeparatedSeaNames = seaNames.join(', ');
+
+        mer_html.innerHTML = commaSeparatedSeaNames;
+        pays_html.innerHTML = information.Countries.Name;
+        console.log(information.Archipelago.Name);
+        
+
+        
     }
 
    
